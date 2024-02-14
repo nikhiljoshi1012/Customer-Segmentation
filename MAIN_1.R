92867b1a8b03c94a036bebc3fa3611a89d1bb4dd
nikhiljoshi1012 <nikhiljoshi1012@gmail.com>
2024-02-14 09:48:10 +0530

    Code fixes Age distribution Visualization of Age Distribution Customer spending

--- MAIN_1.R
+++ MAIN_1.R
@@ -17,3 +17,66 @@ summary(customer_data$Annual.Income..k..)
 sd(customer_data$Annual.Income..k..)
 
 summary(customer_data$Age)
+
+sd(customer_data$Spending.Score..1.100.)
+
+
+#Customer Gender Visualization
+a=table(customer_data$Gender)
+barplot(a,main="Using BarPlot to display Gender Comparision",
+        ylab="Count",
+        xlab="Gender",
+        col=rainbow(2),
+
+              legend=rownames(a))
+
+
+
+
+pct=round(a/sum(a)*100)
+lbs=paste(c("Female","Male")," ",pct,"%",sep=" ")
+library(plotrix)
+pie3D(a,labels=lbs,
+      main="Pie Chart Depicting Ratio of Female and Male")
+
+
+
+summary(customer_data$Age)
+
+
+
+hist(customer_data$Age,
+     col="blue",
+     main="Histogram to Show Count of Age Class",
+     xlab="Age Class",
+     ylab="Frequency",
+     labels=TRUE)
+
+
+boxplot(customer_data$Age,
+        col="purple",
+        main="Boxplot for Descriptive Analysis of Age")
+
+
+
+summary(customer_data$Annual.Income..k..)
+hist(customer_data$Annual.Income..k..,
+     col="#660033",
+     main="Histogram for Annual Income",
+     xlab="Annual Income Class",
+     ylab="Frequency",
+     labels=TRUE)
+
+
+
+plot(density(customer_data$Annual.Income..k..),
+     col="yellow",
+     main="Density Plot for Annual Income",
+     xlab="Annual Income Class",
+     ylab="Density")
+polygon(density(customer_data$Annual.Income..k..),
+        col="#ccff66")
+
+
+
+
