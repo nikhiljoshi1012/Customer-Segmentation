5bed14b7e996856623524833d068d5b6c2f465b6
nikhiljoshi1012 <nikhiljoshi1012@gmail.com>
2024-03-14 07:04:00 +0530

    More data cleaning
    Cleaned more data + made some label changes

--- cleaning.R
+++ cleaning.R
@@ -19,6 +19,9 @@ dirty_data_CD <- dirty_data_CD[!na_occurrences, ]
 dirty_data_CD <- dirty_data_CD[, -which(names(dirty_data_CD) == "deceased_indicator")]
 dirty_data_CD <- dirty_data_CD[, -which(names(dirty_data_CD) == "default")]
 
+dirty_data_CD <- dirty_data_CD[complete.cases(dirty_data_CD), ]
+
+
 # Now you can continue with your analysis or view the updated data frame if needed
 View(dirty_data_CD)
 
@@ -26,7 +29,7 @@ library(dplyr)
 dirty_data_CD<-dirty_data_CD %>%
   rename(CustomerID="customer_id")
 dirty_data_CD<-dirty_data_CD %>%
-  rename(FNAME='first_name',LNAME='last_name',Gender='gender');
+  rename(Fname='first_name',Lname='last_name',Gender='gender');
 
 dirty_data_CD<-dirty_data_CD %>%
   rename(JobTitle="job_title")
@@ -74,6 +77,23 @@ View(dirty_data_TR)
 #####################################
 
 dirty_data_NC <- read_excel("Raw_data.xlsx", sheet = "NewCustomerList")
+# Remove rows with any form of NA values in any column
+dirty_data_NC <- dirty_data_NC[complete.cases(dirty_data_NC), ]
+
+# Check for the occurrence of the substring 'n/a' in the entire sheet
+na_occurrences <- apply(dirty_data_NC, 1, function(row) any(grepl("n/a", as.character(row), ignore.case = TRUE)))
+
+# Subset the data frame to keep only rows without 'n/a' occurrences
+dirty_data_NC <- dirty_data_NC[!na_occurrences, ]
+
+#Dropping the columns with Random values
+dirty_data_NC <- dirty_data_NC[, -which(names(dirty_data_NC) == "deceased_indicator")]
+
+
+library(dplyr)
+
+dirty_data_NC<-dirty_data_NC %>%
+  rename(Fname='first_name',Lname='last_name',Gender='gender',JobTitle="job_title");
 
 View(dirty_data_NC)
 
