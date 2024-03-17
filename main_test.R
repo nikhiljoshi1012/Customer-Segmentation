fde88578921d7bdb82bfae6c36e9d89abaf32a48
nikhiljoshi1012 <nikhiljoshi1012@gmail.com>
2024-03-17 12:18:08 +0530

    Analysis Started
    Cleaning finished and started analysis RFM

---
+++ main_test.R
@@ -0,0 +1,107 @@
+library(dplyr)    # For data manipulation
+library(ggplot2)  # For data visualization
+library(lubridate) # For working with dates and times
+
+# Load required packages
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+
+# Set ggplot2 theme
+theme_set(theme_bw())
+
+# Loading the Transactions and Customer Demographics Datasets
+trans <- read.csv('Transactions_Cleaned.csv')
+cust <- read.csv('CustomerDemographic_Cleaned.csv')
+
+
+
+head(trans) # Will display the first 6 rows by default
+tail(trans) # Will display the last 6 rows by default
+
+
+# Total records (rows) in the Transaction Dataset
+cat("Total records (rows) in the Transaction Dataset:", nrow(trans), "\n")
+
+# Total features (columns) in the Transaction Dataset
+cat("Total features (columns) in the Transaction Dataset:", ncol(trans), "\n")
+
+head(cust)  # Displays the first 6 rows
+tail(cust)  # Displays the last 6 rows
+
+# Total records (rows) in the Customer Demographics Dataset
+cat("Total records (rows) in the Customer Demographics Dataset:", nrow(cust), "\n")
+
+# Total features (columns) in the Customer Demographics Dataset
+cat("Total features (columns) in the Customer Demographics Dataset:", ncol(cust), "\n")
+
+library(dplyr)
+
+merged_trans_cust <- inner_join(trans, cust, by = "customer_id")
+
+head(merged_trans_cust)  # Displays the first 6 rows
+tail(merged_trans_cust)  # Displays the last 6 rows
+
+
+
+# Total records (rows) in the Merged Dataset
+cat("Total records (rows) in the Merged Dataset:", nrow(merged_trans_cust), "\n")
+
+# Total features (columns) in the Merged Dataset
+cat("Total features (columns) in the Merged Dataset:", ncol(merged_trans_cust), "\n")
+
+
+summary(merged_trans_cust)
+
+
+library(lubridate)  # Load the lubridate package
+
+# Convert transaction_date column to Date format
+merged_trans_cust$transaction_date <- ymd(merged_trans_cust$transaction_date)
+
+########################################RFM ANALYSIS#########################################################
+
+
+library(lubridate)
+# Maximum Transaction Date
+max_trans_date <- max(merged_trans_cust$transaction_date)
+max_trans_date
+
+
+library(lubridate)
+
+# Maximum Transaction Date
+max_trans_date <- max(merged_trans_cust$transaction_date)
+# Convert max_trans_date to a Date object
+comparison_date <- as.Date(max_trans_date)
+
+
+
+library(dplyr)
+
+# Assuming 'comparison_date' is a variable containing the desired date for comparison
+rfm_table <- merged_trans_cust %>%
+  group_by(customer_id) %>%
+  summarise(
+    recency = as.numeric(comparison_date - max(transaction_date), units = "days"),
+    frequency = n_distinct(product_id),
+    monetary = sum(Profit, na.rm = TRUE)
+    
+  )
+
+column_names <- names(rfm_table)
+print(column_names)
+
+print(names(rfm_table))
+
+library(dplyr)
+
+rfm_table <- rfm_table %>%
+  rename(
+    transaction_date = recency,
+    product_id = frequency,
+    Profit = monetary
+  )
+
+
+
